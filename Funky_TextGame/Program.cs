ad24954aadc0adaa1541466b2f58cee9ef69e754
t013450n <t013450n@C115-06>
2023-10-02 16:13:21 +0100

    Add project files.

---
+++ Funky_TextGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tutorial_Week_1_PF
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+
+}
